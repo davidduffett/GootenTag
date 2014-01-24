fca1216418f9c93261a472c0e55ba88a794d5be0
David Duffett <duffett.david@gmail.com>
2014-01-24 15:58:10 +1100

    MVC example for Google Tag Manager

---
+++ GootenTag/GootenTag.Example/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace GootenTag.Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        [GoogleTagManagerVariable("page_type", "home")]
+        public ActionResult Index()
+        {
+            GoogleTagManager.Current.DataLayer.CustomerId = Guid.NewGuid();
+            GoogleTagManager.Current.DataLayer.TestString = "GootenTag's test";
+            GoogleTagManager.Current.DataLayer.TestNumber = 123;
+            GoogleTagManager.Current.DataLayer.TestArray = new[] { "value1", "value2", "value3" };
+            return View();
+        }
+    }
+}
